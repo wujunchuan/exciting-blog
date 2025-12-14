8edec13872bd8c57ca7fc76c16182bfc7c0dff2a
John Trump <wujunchuan2008@gmail.com>
2025-12-14 22:46:47 +0800

    fix: Update site language from 'en' to 'zh_CN' for localization

--- src/config.ts
+++ src/config.ts
@@ -11,7 +11,7 @@ import { LinkPreset } from "./types/config";
 export const siteConfig: SiteConfig = {
 	title: "Aloha~Waii",
 	subtitle: "苟利国家生死以，岂因祸福避趋之。",
-	lang: "en", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
+	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
 		fixed: false, // Hide the theme color picker for visitors
